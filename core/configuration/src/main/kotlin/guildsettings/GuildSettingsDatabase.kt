43eea06f5a0fbed00b9a5deafac7231a9c642215
Daniele Segato <daniele.segato@neosperience.com>
2022-12-04 12:57:10 +0100

    add a comment

--- core/configuration/src/main/kotlin/guildsettings/GuildSettingsDatabase.kt
+++ core/configuration/src/main/kotlin/guildsettings/GuildSettingsDatabase.kt
@@ -2,6 +2,7 @@ package guildsettings
 
 import kotlinx.coroutines.flow.Flow
 
+// TODO write higher level functions to alter the DB and expose those
 interface GuildSettingsDatabase {
     fun getString(guildId: String, key: String): Flow<String?>
 
