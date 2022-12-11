fe790fe985558e1f551662b6acb7817f2b0bc643
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-12-11 21:05:19 +1300

    Further simplify test code

--- core/scheduler/src/main/kotlin/Schedule.kt
+++ core/scheduler/src/main/kotlin/Schedule.kt
@@ -2,12 +2,12 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
 import kotlinx.datetime.toInstant
-import kotlinx.datetime.toJavaLocalDateTime
-import kotlinx.datetime.toKotlinLocalDateTime
 import kotlinx.datetime.toLocalDateTime
 import kotlin.coroutines.coroutineContext
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
 
 suspend fun schedule(
     repeating: Repeating,
@@ -24,42 +24,19 @@ private fun calculateDelayUntilNextExecution(clock: Clock, repeating: Repeating)
     val timeZone = TimeZone.currentSystemDefault()
     val nowDateTime = clock.now().toLocalDateTime(timeZone)
 
-    // TODO Don't convert to java LocalDateTime
-    val nextDateTime = nowDateTime.toJavaLocalDateTime()
-        .withHour(repeating.hour)
-        .withMinute(repeating.minute)
-        .withSecond(repeating.second)
-        .toKotlinLocalDateTime()
+    val nextDateTime = nowDateTime.date
+        .atTime(repeating.hour, repeating.minute, repeating.second)
 
-    // Add any required offset
-    val nextExecutionDateTime = when (repeating) {
-        is Repeating.Daily -> {
-            if (nextDateTime <= nowDateTime) {
-                nextDateTime.toJavaLocalDateTime()
-                    .plusDays(1)
-                    .toKotlinLocalDateTime()
-            } else {
-                nextDateTime
-            }
+    val nextInstant = if (nextDateTime <= nowDateTime) {
+        val extraDays = when (repeating) {
+            is Repeating.Daily -> 1
+            is Repeating.Weekly -> 7
+            is Repeating.Fortnightly -> 14
         }
-        is Repeating.Weekly -> {
-            if (nextDateTime <= nowDateTime) {
-                nextDateTime.toJavaLocalDateTime()
-                    .plusDays(7)
-                    .toKotlinLocalDateTime()
-            } else {
-                nextDateTime
-            }
-        }
-        is Repeating.Fortnightly -> {
-            if (nextDateTime <= nowDateTime) {
-                nextDateTime.toJavaLocalDateTime()
-                    .plusDays(14)
-                    .toKotlinLocalDateTime()
-            } else {
-                nextDateTime
-            }
-        }
+        nextDateTime.toInstant(timeZone)
+            .plus(extraDays.days)
+    } else {
+        nextDateTime.toInstant(timeZone)
     }
-    return nextExecutionDateTime.toInstant(timeZone) - nowDateTime.toInstant(timeZone)
+    return nextInstant - nowDateTime.toInstant(timeZone)
 }
