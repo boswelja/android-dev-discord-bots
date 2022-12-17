b6b80224a8b794f3077367c45875e8d228cd8751
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-12-17 14:46:50 +1300

    Count executions for repeating tests

--- core/scheduler/src/test/kotlin/ScheduleTest.kt
+++ core/scheduler/src/test/kotlin/ScheduleTest.kt
@@ -1,10 +1,7 @@
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.advanceTimeBy
-import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
@@ -12,11 +9,7 @@ import kotlinx.datetime.toLocalDateTime
 import org.junit.jupiter.api.Test
 import java.time.DayOfWeek
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
-import kotlin.time.Duration
 import kotlin.time.Duration.Companion.hours
-import kotlin.time.Duration.Companion.milliseconds
-import kotlin.time.Duration.Companion.seconds
 
 class ScheduleTest {
 
@@ -114,36 +107,22 @@ class ScheduleTest {
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    private suspend fun TestScope.testScheduleRepeating(
-        repeating: Repeating
+    private fun TestScope.testScheduleRepeating(
+        repeating: Repeating,
+        targetRuns: Int = 5
     ) {
-        var runCount = 0
-        var lastRunTime = 0L
-        schedule(
-            repeating = repeating
-        ) {
-            val millisSinceLastRun = currentTime - lastRunTime
-
-            if (runCount > 5) {
-                // If we've done more than 5 runs, cancel
-                coroutineScope { cancel() }
-            } else if (runCount > 0) {
-                // Else if we have passed one run, start asserting
-                assertEqualsApproximately(
-                    repeating.interval,
-                    millisSinceLastRun.milliseconds,
-                    1.seconds
-                )
+        // Wrap in a Launch, so we can cancel after the first execution
+        var executions = 0
+        val scheduleJob = launch {
+            schedule(
+                repeating = repeating
+            ) {
+                executions++
             }
-
-            // Increment run count
-            runCount++
-            lastRunTime = currentTime
         }
-    }
 
-    private fun assertEqualsApproximately(expected: Duration, actual: Duration, tolerance: Duration) {
-        val range = (expected - tolerance)..(expected + tolerance)
-        assertTrue(range.contains(actual), "Expected duration within $tolerance of $expected, but got $actual")
+        advanceTimeBy(repeating.interval.inWholeMilliseconds * targetRuns)
+        assertEquals(targetRuns, executions)
+        scheduleJob.cancel()
     }
 }
