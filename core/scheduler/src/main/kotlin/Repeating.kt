f3db48154fac88754fa8c583281dbeca936bbfce
Daniele Segato <daniele.segato@neosperience.com>
2022-12-18 11:19:45 +0100

    Run spotless apply

--- core/scheduler/src/main/kotlin/Repeating.kt
+++ core/scheduler/src/main/kotlin/Repeating.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 AndroidDev Discord Dev Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import java.time.DayOfWeek
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.days
@@ -33,7 +48,7 @@ sealed interface Repeating {
     data class Daily(
         override val hour: Int = 0,
         override val minute: Int = 0,
-        override val second: Int = 0
+        override val second: Int = 0,
     ) : Repeating {
         override val interval: Duration = 1.days
     }
@@ -46,7 +61,7 @@ sealed interface Repeating {
         val weekday: DayOfWeek,
         override val hour: Int = 0,
         override val minute: Int = 0,
-        override val second: Int = 0
+        override val second: Int = 0,
     ) : Repeating {
         override val interval: Duration = 7.days
     }
@@ -59,7 +74,7 @@ sealed interface Repeating {
         val weekday: DayOfWeek,
         override val hour: Int = 0,
         override val minute: Int = 0,
-        override val second: Int = 0
+        override val second: Int = 0,
     ) : Repeating {
         override val interval: Duration = 14.days
     }
