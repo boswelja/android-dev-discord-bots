7817c125075f779e468fa53c9a38ceb35b1ea3f2
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-01-06 22:59:57 +1300

    Add Kord forums snapshot

--- core/scheduler/build.gradle.kts
+++ core/scheduler/build.gradle.kts
@@ -6,13 +6,9 @@ plugins {
 group = "com.boswelja"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    api("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
     testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
