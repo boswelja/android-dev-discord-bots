f0bfb58c191b850730cc947332f4e4c03443cdfb
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-02-25 21:19:17 +1300

    Remove Kord impl

--- core/discord/build.gradle.kts
+++ core/discord/build.gradle.kts
@@ -8,7 +8,6 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     api(libs.kotlinx.coroutines.core)
-    implementation(libs.bundles.kord)
     implementation(libs.jda) {
         exclude(module = "opus-java") // We don't need audio components (yet)
     }
