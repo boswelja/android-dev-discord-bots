2f42104b558ad5253b9d462425046d157e4c6950
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-08-19 15:38:20 +1200

    Try to fix resolution overload

--- features/android-studio-updates/src/main/kotlin/features/updates/androidstudio/updatesource/AndroidStudioBlogUpdateSource.kt
+++ features/android-studio-updates/src/main/kotlin/features/updates/androidstudio/updatesource/AndroidStudioBlogUpdateSource.kt
@@ -20,6 +20,7 @@ import features.updates.androidstudio.rssfetcher.Fetcher
 import features.updates.androidstudio.rssfetcher.FetcherFactory
 import kotlinx.datetime.Instant
 import kotlinx.datetime.toKotlinInstant
+import kotlin.streams.toList
 
 /**
  * An implementation of [AndroidStudioUpdateSource] that retrieves updates from the Android Studio Blog.
