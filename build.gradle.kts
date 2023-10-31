d340ad2b72dd787c4d6c8a97871142003cfc525e
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-10-31 13:14:04 +1300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0" apply false
+    kotlin("jvm") version "1.9.20" apply false
     id("app.cash.sqldelight") version "2.0.0" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.1" apply false
 }
