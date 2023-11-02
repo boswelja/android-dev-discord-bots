9a6868943816508afc0ff176163d15a0f1de4d9f
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-11-02 21:05:20 +1300

    Fix long line

--- features/android-studio-updates/src/main/kotlin/features/updates/androidstudio/configuration/AndroidStudioUpdateSettingsDatabase.kt
+++ features/android-studio-updates/src/main/kotlin/features/updates/androidstudio/configuration/AndroidStudioUpdateSettingsDatabase.kt
@@ -33,7 +33,8 @@ class AndroidStudioUpdateSettingsDatabase(
         channelSettings.delete(channelId, TargetChannelKey)
     }
 
-    override suspend fun getAllTargetChannels(): List<String> = channelSettings.getChannelsWhere(TargetChannelKey, "").first()
+    override suspend fun getAllTargetChannels(): List<String> =
+        channelSettings.getChannelsWhere(TargetChannelKey, "").first()
 
     companion object {
         private const val TargetChannelKey = "target_channel"
