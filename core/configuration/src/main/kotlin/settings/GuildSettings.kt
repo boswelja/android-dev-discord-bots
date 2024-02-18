6fc76122825b7781e317d8901bac1dc9890dd885
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-02-18 14:33:50 +1300

    Update all license files

--- core/configuration/src/main/kotlin/settings/GuildSettings.kt
+++ core/configuration/src/main/kotlin/settings/GuildSettings.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 AndroidDev Discord Dev Team
+ * Copyright 2024 AndroidDev Discord Dev Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
